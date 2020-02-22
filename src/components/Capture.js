30661dac565b37e2be45182aff7680bb58b0b9ff
Berk <alexeym00@gmail.com>
2020-02-22 16:28:13 +0200

    render all cams

--- src/components/Capture.js
+++ src/components/Capture.js
@@ -5,36 +5,68 @@ import Webcam from "react-webcam";
 // width = 1280;
 
 
-const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user"
-  };
+// const videoConstraints = {
+//     width: 1280,
+//     height: 720,
+//     facingMode: "user"
+//   };
   
-  const WebcamCapture = () => {
-    const webcamRef = React.useRef(null);
+//   const WebcamCapture = () => {
+//     const webcamRef = React.useRef(null);
   
-    const capture = React.useCallback(
-      () => {
-        const imageSrc = webcamRef.current.getScreenshot();
-        console.log(imageSrc)
-      },
-      [webcamRef]
-    );
+//     const capture = React.useCallback(
+//       () => {
+//         const imageSrc = webcamRef.current.getScreenshot();
+//         console.log(imageSrc)
+//       },
+//       [webcamRef]
+//     );
   
-    return (
-      <>
-        <Webcam
-          audio={false}
-          height={720}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          width={1280}
-          videoConstraints={videoConstraints}
-        />
-        <button onClick={capture}>Capture photo</button>
-      </>
-    );
-  };
+//     return (
+//       <>
+//         <Webcam
+//           audio={false}
+//           height={720}
+//           ref={webcamRef}
+//           screenshotFormat="image/jpeg"
+//           width={1280}
+//           videoConstraints={videoConstraints}
+//         />
+//         <button onClick={capture}>Capture photo</button>
+//       </>
+//     );
+//   };
+
+
+
+const WebcamCapture = () => {
+  const [deviceId, setDeviceId] = React.useState({});
+  const [devices, setDevices] = React.useState([]);
+
+  const handleDevices = React.useCallback(
+    mediaDevices =>
+      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
+    [setDevices]
+  );
+
+  React.useEffect(
+    () => {
+      navigator.mediaDevices.enumerateDevices().then(handleDevices);
+    },
+    [handleDevices]
+  );
+
+  return (
+    <>
+      {devices.map((device, key) => (
+          <div>
+            <Webcam audio={false} videoConstraints={{ deviceId: device.deviceId }} />
+            {device.label || `Device ${key + 1}`}
+          </div>
+
+        ))}
+    </>
+  );
+};
 
 export default WebcamCapture;
