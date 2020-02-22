e5c041d03083526583517a8e317b4f2a4960b295
Alex Shevchuk <spichka.vin@gmail.com>
2020-02-22 15:36:00 +0200

    Header demmo

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './../resourses/logo.svg';
+import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from 'react-router-dom'
+
+import List from './list/List'
+import NotFound from './error/404'
+import Header from './header/Header'
 import './../styles/App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={List}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Router>
     </div>
+
   );
 }
 
